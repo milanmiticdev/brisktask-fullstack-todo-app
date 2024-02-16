bebbfd9005f2d913856563ae2cecd4fcb4404109
Milan Mitic <128543445+milanmiticdev@users.noreply.github.com>
2024-02-16 14:31:26 +0100

    Fixing minor controller and routes issues

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,11 @@
 import express from 'express';
 import helmet from 'helmet';
 import cors from 'cors';
+import config from './config/config.js';
+
+import authRouter from './routes/auth.router.js';
+import taskRouter from './routes/task.router.js';
+import userRouter from './routes/user.router.js';
 
 const app = express();
 
@@ -21,6 +26,10 @@ app.use(
 	})
 );
 
-app.use('auth');
-app.use('/users');
-app.use('/tasks');
+app.use('/auth', authRouter);
+app.use('/users', taskRouter);
+app.use('/tasks', userRouter);
+
+const { serverHostname, serverPort } = config;
+
+app.listen(serverPort, () => `Server started on ${serverHostname}:${serverPort}`);
