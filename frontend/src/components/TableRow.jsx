a280383f5a7b9d8b1b22f9f362ffa70217089bdb
Milan Mitic <128543445+milanmiticdev@users.noreply.github.com>
2024-02-28 14:56:46 +0100

    admin dashboard tables and selection

--- frontend/src/components/TableRow.jsx
+++ frontend/src/components/TableRow.jsx
@@ -1,12 +1,3 @@
-//React
-import { useContext } from 'react';
-
-// React Router
-import { useNavigate } from 'react-router-dom';
-
-// Context
-import AuthContext from '../contexts/AuthContext.js';
-
 // Font Awesome Icons
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEye } from '@fortawesome/free-solid-svg-icons';
@@ -17,39 +8,7 @@ import styles from './TableRow.module.css';
 // PropTypes
 import PropTypes from 'prop-types';
 
-const TableRow = ({ category, parent, id, email, dispatch }) => {
-	const navigate = useNavigate();
-	const { token } = useContext(AuthContext);
-
-	const deleteRow = async () => {
-		try {
-			dispatch({ type: 'loading-check', payload: true });
-			dispatch({ type: 'spinner-text-change', payload: 'Deleting' });
-
-			const response = await fetch(`http://localhost:5174/api/v1/${category}/${id}`, {
-				method: 'DELETE',
-				body: null,
-				headers: {
-					Authorization: `Bearer ${token}`,
-				},
-			});
-
-			if (response.status === 204) {
-				navigate(0);
-			} else {
-				const data = await response.json();
-
-				dispatch({ type: 'spinner-text-change', payload: '' });
-				dispatch({ type: 'loading-check', payload: false });
-				dispatch({ type: 'modal-change', payload: { isOpen: true, error: true, message: data.message } });
-			}
-		} catch {
-			dispatch({ type: 'spinner-text-change', payload: '' });
-			dispatch({ type: 'loading-check', payload: false });
-			dispatch({ type: 'modal-change', payload: { isOpen: true, error: true, message: 'Something went wrong.' } });
-		}
-	};
-
+const TableRow = ({ category, parent, id, email }) => {
 	return (
 		<>
 			{parent === 'head' ? (
@@ -80,5 +39,4 @@ TableRow.propTypes = {
 	parent: PropTypes.string,
 	id: PropTypes.number,
 	email: PropTypes.string,
-	dispatch: PropTypes.func,
 };
