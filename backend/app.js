5fc302e3d767e8a4b22cf8a7f768f81cba706c02
Milan Mitic <128543445+milanmiticdev@users.noreply.github.com>
2024-03-02 20:37:36 +0100

    minor updates

--- backend/app.js
+++ backend/app.js
@@ -37,3 +37,5 @@ app.use(express.static(path.join(__dirname, '/frontend/dist')));
 app.get('*', (req, res) => {
 	res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
 });
+
+app.listen(5174);
